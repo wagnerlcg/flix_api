0561e48ea7d5b781be6bf1385e3227441c0169a7
InfoSulF Soluções em Informática <wagnerlcg@gmail.com>
2024-10-29 17:51:46 -0300

    auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+app_name = 'authentication'
 
+
+# urlpatterns = [
+#     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+# ]
 urlpatterns = [
-    path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
